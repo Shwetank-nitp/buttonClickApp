30d6dda4e029b7e3445a91126730ae761fb46e8f
Shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2023-09-02 19:40:27 +0530

    buttonClickApp

--- app/src/main/java/shrai/ee/buttonclickapp/MainActivity.kt
+++ app/src/main/java/shrai/ee/buttonclickapp/MainActivity.kt
@@ -2,7 +2,6 @@ package shrai.ee.buttonclickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.widget.Button
