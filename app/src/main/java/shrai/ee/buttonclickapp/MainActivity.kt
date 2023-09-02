acbed92048bc2adbf6a6cfe28cfb5434ce8e48c6
Shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2023-09-02 19:39:24 +0530

    buttonClickApp

--- app/src/main/java/shrai/ee/buttonclickapp/MainActivity.kt
+++ app/src/main/java/shrai/ee/buttonclickapp/MainActivity.kt
@@ -2,31 +2,43 @@ package shrai.ee.buttonclickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-
 class MainActivity : AppCompatActivity() {
-    private var button:Button? = null
-    private var userInput:EditText? = null
+    private val TAG = "MainActivity"
+    private var STORE_TEXT = ""
     private var textView:TextView? = null
-    private var numTimesClicked:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        userInput = findViewById(R.id.editTextTextPersonName)
-        button = findViewById(R.id.button)
+        Log.d(TAG, "onCreate: called")
+        val userInput:EditText? = findViewById(R.id.editTextTextPersonName)
+        val button:Button? = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
         textView?.text=""
         textView?.movementMethod = ScrollingMovementMethod()
         button?.setOnClickListener {
-            numTimesClicked++
-            textView?.append("Hi ${userInput?.text} clicked button $numTimesClicked time")
-            if (numTimesClicked!=1)
-                textView?.append("s\n")
-            else textView?.append("\n")
+            Log.d(TAG, "onClick: called")
+            if (!userInput?.text.isNullOrBlank()){
+                textView?.append("${userInput?.text}\n")
+                userInput?.text?.clear()
+            }
         }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG,"OnSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+        outState.putString(STORE_TEXT, textView?.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState.getString(STORE_TEXT,"")
     }
 }
